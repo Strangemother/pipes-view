54f907bec70dea81562f8cf6a363d3e1268f0d80
Jay <github@strangemother.com>
2026-05-04 02:09:10 +0000

    Refactor Stepper class and reintroduce GraphWalker with enhanced connection management

--- server_v2/static/js/src/graph-stepper.js
+++ server_v2/static/js/src/graph-stepper.js
@@ -13,6 +13,11 @@ class Stepper {
 
     constructor(graph) {
         this.graph = graph
+        this.origin = undefined
+        this.rows = []
+        this.stash = {}
+        this._history = []
+        this._nextNodes = []
         this._current = undefined;
     }
 
@@ -21,31 +26,153 @@ class Stepper {
     }
 
     start() {
-        this._current = this._current || this.origin
-        this.step.apply(this, arguments)
+        return this.step.apply(this, arguments)
     }
 
     step() {
-        /* Perform an auto step of the internals */
-        let place = this._current
+        if(this.rows.length == 0) {
+            if(this.origin == undefined) {
+                return []
+            }
 
-        let node = this.graph.getNode(place)
-        let nextNodes = this.graph.getNextNodesDict(place)
-        this._nextNodes = Object.keys(nextNodes)
-        this.runTarget(place, node, nextNodes, Array.from(arguments))
+            this.rows = [
+                this.createRow(
+                    this.origin,
+                    this.createTrackedValue(this.normalizeInput(Array.from(arguments)))
+                )
+            ]
+        }
+
+        let currentRows = this.rows
+        let nextRows = []
+
+        this.rows = []
+        this._nextNodes = []
+
+        for(let row of currentRows) {
+            let place = row.nodeName
+            let node = this.graph.getNode(place)
+            let nextNodes = this.graph.getNextNodesDict(place) || {}
+            let nextNodeNames = Object.keys(nextNodes)
+            let execution = this.runTarget(place, node, nextNodes, row.input)
+
+            if(nextNodeNames.length == 0) {
+                this.stashResult(place, execution)
+                continue
+            }
+
+            for(let nextNodeName of nextNodeNames) {
+                nextRows.push(this.createRow(nextNodeName, execution))
+            }
+        }
+
+        this.rows = nextRows
+        this._nextNodes = nextRows.map((row) => row.nodeName)
+        this._current = this._nextNodes[0]
+
+        return nextRows
     }
 
-    runTarget(nodeName, node, nextNodes, args) {
+    createRow(nodeName, input) {
+        return {
+            nodeName: nodeName,
+            input: input,
+        }
+    }
+
+    createTrackedValue(value) {
+        return this.trackPromise(Promise.resolve(value))
+    }
+
+    trackPromise(promise) {
+        let tracked = {
+            promise: undefined,
+            settled: false,
+            fulfilled: false,
+            rejected: false,
+            value: undefined,
+            error: undefined,
+        }
+
+        tracked.promise = Promise.resolve(promise).then(
+            (value) => {
+                tracked.settled = true
+                tracked.fulfilled = true
+                tracked.value = value
+                return value
+            },
+            (error) => {
+                tracked.settled = true
+                tracked.rejected = true
+                tracked.error = error
+                throw error
+            }
+        )
+
+        return tracked
+    }
+
+    normalizeInput(args) {
+        if(args.length == 0) {
+            return undefined
+        }
+
+        if(args.length == 1) {
+            return args[0]
+        }
+
+        return args
+    }
+
+    stashResult(nodeName, execution) {
+        execution.promise.then(
+            (value) => {
+                let items = this.stash[nodeName]
+                if(items == undefined) {
+                    items = []
+                    this.stash[nodeName] = items
+                }
+
+                items.push(value)
+                return value
+            },
+            (error) => {
+                console.error('Step failed:', nodeName, error)
+            }
+        )
+    }
+
+    runTarget(nodeName, node, nextNodes, input) {
         this._current = nodeName
         console.log(
             'Run Node:', node,
-            '\nWith Args:', args,
+            '\nWith Promise:', input?.promise || input,
             '\nThen Next:', nextNodes,
             )
 
         /* Execute */
-        let res = node.apply(this, args)
-        console.log('Result:', res)
+        let execution = this.trackPromise(
+            Promise.resolve().then(() => node.call(this, input.promise))
+        )
+
+        execution.promise.then(
+            (value) => {
+                console.log('Result:', value)
+                return value
+            },
+            (error) => {
+                console.error('Result Error:', error)
+            }
+        )
+
+        this._history.push({
+            nodeName: nodeName,
+            nextNodes: Object.keys(nextNodes),
+            input: input,
+            execution: execution,
+        })
+
+        return execution
     }
 
 
